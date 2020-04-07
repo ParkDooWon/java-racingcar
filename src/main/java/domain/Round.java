06e04b290021c7d3a5fa63bc5ce0beaf392dbc10
ParkDooWon <50234855+ParkDooWon@users.noreply.github.com>
2020-04-07 23:28:11 +0900

    refactor : round의 캐싱된 map 이름 변경

--- src/main/java/domain/Round.java
+++ src/main/java/domain/Round.java
@@ -13,13 +13,14 @@ import domain.exception.InvalidRoundException;
 public class Round {
 	private static final int GAME_FINISH = 0;
 	private static final int REDUCE_ROUND_NUMBER = 1;
-	private static final Map<Integer, Round> roundMap = new HashMap<>();
+	private static final Map<Integer, Round> cacheRound = new HashMap<>();
+	//사용자가 많아질 것을 고려하여 round까지 캐싱해서 사용하는 것이 과한가?
 
 	private final int round;
 
 	static {
 		for (int i = 0; i <= 100; i++) {
-			roundMap.put(i, new Round(i));
+			cacheRound.put(i, new Round(i));
 		}
 	}
 
@@ -30,10 +31,10 @@ public class Round {
 	public static Round of(String inputRound) {
 		validateNumber(inputRound);
 		int round = Integer.parseInt(inputRound);
-		if (roundMap.get(round) == null) {
-			roundMap.put(round, new Round(round));
+		if (cacheRound.get(round) == null) {
+			cacheRound.put(round, new Round(round));
 		}
-		return roundMap.get(round);
+		return cacheRound.get(round);
 	}
 
 	private static void validateNumber(String round) {
@@ -50,6 +51,6 @@ public class Round {
 
 	public Round reduceRound() {
 		int reduceRound = this.round - REDUCE_ROUND_NUMBER;
-		return roundMap.get(reduceRound);
+		return cacheRound.get(reduceRound);
 	}
 }
